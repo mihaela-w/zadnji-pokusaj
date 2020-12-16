780b3857222757250e8df748ea8ef43110fb7a6b
mihaela-w <maite.miwa@gmail.com>
2020-12-16 14:21:52 +0100

    Creating Combobox: Internal logic

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
  
 import { languages } from '../../constants/const'
 import ComboboxItem from './ComboboxItem'
 import styles from './style.module.css' 
  
-const Combobox = () => (
-    <div className={styles.combobox}>
-        {<ul>
-          {languages.map(({language, name, flag}) => 
-            <ComboboxItem language={language}
-              name={name} 
-              flag={flag} />)}
-        </ul>
-        }
-    </div>
-  )
+const Combobox = () => {
+    const [selectedItem, setSelectedItem] = useState({}) // dodajemo prazan objekt 
+    const [open, setOpen] = useState(true)
+    return (
+        <div className={styles.combobox} onClick={() => setOpen(!open) }>
+            {<ul style={{display: open ? 'flex' : 'none'}}>
+    {languages.map(el => 
+    <ComboboxItem {...el} 
+      setSelected={setSelectedItem}
+      selected={el.name === selectedItem.name}
+    />)}
+</ul>}
+            {selectedItem.language}
+        </div>
+    )
+  }  
  
 export default Combobox
