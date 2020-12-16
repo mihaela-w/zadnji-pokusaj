10df5a577e5ba4b696ba067f95905423ae92f616
mihaela-w <maite.miwa@gmail.com>
2020-12-16 17:00:27 +0100

    configuring ImagePool

--- src/components/ImagePool/index.js
+++ src/components/ImagePool/index.js
@@ -1,34 +1,37 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import styles from "./style.module.css"
- 
-const ImagePool = () => {
-    const data = useStaticQuery(graphql`
-        query {
-          myImages: allInstaNode(limit: 24, filter: {mediaType: {eq: "GraphImage"}}) {
-            edges {
-              node {
-                localFile {
-                  childImageSharp {
-                    fixed(width: 280 height: 190) {
-                        ...GatsbyImageSharpFixed
-                    }
-                  }
+
+const ImagePool = ({ start, end }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      myImages: allInstaNode(
+        limit: 24
+        filter: { mediaType: { eq: "GraphImage" } }
+      ) {
+        edges {
+          node {
+            localFile {
+              childImageSharp {
+                fixed(width: 320, height: 190) {
+                  ...GatsbyImageSharpFixed
                 }
               }
             }
           }
         }
-    `)
-    return (
-    <div>
-        {data.myImages.edges.map(edge =>
-          <Img fixed={edge.node.localFile.childImageSharp.fixed} />
-        )}
+      }
+    }
+  `)
+  return (
+    <div className={styles.imagePool}>
+      {data.myImages.edges.slice(start, end).map(edge => (
+        <Img fixed={edge.node.localFile.childImageSharp.fixed} />
+      ))}
     </div>
-    )
+  )
 }
- 
+
 export default ImagePool
