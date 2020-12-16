4b5c2a62ddcb7ac20bdcec4e9da3145810fbc446
mihaela-w <maite.miwa@gmail.com>
2020-12-16 16:26:49 +0100

    ContactSeparator refactor and adding PhotoGallery page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,13 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
-import ContactSeparator from '../modules/ContactSeparator'
+import SeparatorBar from '../modules/SeparatorBar'
 import ContactForm from '../modules/ContactForm'
-
+ 
 const ContactPage = () => (
-    <HeaderFooterLayout activeTab="Contact">
-        <ContactSeparator />
+    <HeaderFooterLayout>
+        <SeparatorBar text="Contact"/>
         <ContactForm />
     </HeaderFooterLayout>
 )
-
+ 
 export default ContactPage
