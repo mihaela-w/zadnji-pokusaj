34ab6ad208c83f794f20635cd15399060ab9ef7d
mihaela-w <maite.miwa@gmail.com>
2021-01-10 16:18:01 +0100

    Show profile and logout in navigation

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { navs as navTabs } from '../../constants/const'
 import styles from './style.module.css'
 import { Link } from 'gatsby'
-
+ 
+const loggedIn = () => !!localStorage.getItem('loggedIn')
+ 
 const NavigationBar = ({ activeTab, useThisStyle }) => (
     <nav className={styles[useThisStyle || 'navigationBar']}>
         {navTabs.map(({tab, to}) => (
@@ -10,7 +12,17 @@ const NavigationBar = ({ activeTab, useThisStyle }) => (
               <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
           </Link>)
         )}
+        <Link to={'/login'}>
+            <li onClick={loggedIn() ? () => localStorage.removeItem('loggedIn') : () => {}}>
+                {loggedIn() ? 'Logout' : 'Login'}
+            </li>
+        </Link>
+        {loggedIn() && <Link to={'/profile'}>
+            <li>
+                Profile
+            </li>
+        </Link>}
     </nav>
 )
-
+ 
 export default NavigationBar
