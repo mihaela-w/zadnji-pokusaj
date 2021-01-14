c3146f4a4858443d31411efbe8ff3a2a2a8882a0
mihaela-w <maite.miwa@gmail.com>
2021-01-14 11:02:10 +0100

    branch2 changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,14 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
 import PageContent from "../modules/PageContent"
- 
+
+
+
+//promjena za branch
+
+
+
+
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     <TitlePage />
